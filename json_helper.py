676788143c6aa8d7a9ca6c7fd9a182cc1af67c73
Preeti Sehgal <preetisehgal2001@gmail.com>
2020-04-20 00:21:55 -0400

    read json file function write and load pickle function

--- json_helper.py
+++ json_helper.py
@@ -1,50 +1,44 @@
-import json
 import os
 import pickle
+from typing import Dict
+import json
 
-def read_json(path):
-    file_name = path
-    with open(file_name) as f:
-        data = json.load(f)
+
+def read_json(file_path: str) -> Dict:
+    with open(file_path) as f:
+        json_input = json.load(f)
+    return json_input
+
+
+def read_all_json_files(json_root):
+    for root, _, files in os.walk(json_root):
+        result = []
+        for f in files:
+            if f.endswith('.json'):
+                json_content = read_json(os.path.join(json_root, f))
+                result.append(json_content)
+    return result
+
+
+def write_pickle(file_path, data):
+    with open(file_path, "wb") as handler:
+        pickle.dump(data, handler)
+
+
+def load_pickle(file_path):
+    with open(file_path, 'rb') as handler:
+        data = pickle.load(handler)
     return data
 
 
-def read_all_json_files(path):
-    all_dicts = []
-    for full_file in os.listdir(path):
-        # full_filename = os.path.join(path, path, full_file)
-        full_filename = os.path.join(path, full_file)
-        with open(full_filename, 'r') as fi:
-            dictionary = json.load(fi)
-            all_dicts.append(dictionary)
-    return all_dicts
+if __name__ == "__main__":
 
+    # Part 2 Proof
 
-def write_pickle(path, data):
-    path = os.path.join(path, 'super_smash_characters.pickle')
-    outfile = open(path, 'wb')
-    pickle_one = pickle.dump(data, outfile, protocol=1)
-    outfile.close()
-    return pickle_one
-    # print(pickle_one)
+    content = read_all_json_files('./data/marvel')
+    print(content)
 
+    write_pickle("marvel.pickle", content)
+    marvel_content = load_pickle("marvel.pickle")
+    print(marvel_content)
 
-#
-def load_pickle(pickle_one):
-    path = open('super_smash_characters.pickle', 'wb')
-    pickle.dumps(int(pickle_one), path)
-    print(path)
-#
-write_pickle('/Users/rmaiale/dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/', {
-    "name": "Link",
-    "neutral_special": "Bow and Arrows",
-    "side_special": " Boomerang",
-    "up_special": " Spin Attack",
-    "down_special": "Remote Bomb",
-    "final_smash": "Ancient Bow and Arrow"
-})
-# load_pickle('/Users/rmaiale/dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/')
-# read_json('/Users/rmaiale/dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/mario.json')
-# read_all_json_files('/Users/rmaiale
-# /dev/PythonFundamentals.Exercises.Part9/data/super_smash_bros/')
-# opening = input("Please ")
